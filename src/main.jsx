27689a261952219bef4fff894233ad3e6c1a5eac
Romina Osorio <r.osorio.marmolejo@gmail.com>
2024-09-09 14:18:59 -0300

    crea AuthContext y ProtectRoute

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx'
 import './main.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { HashRouter } from 'react-router-dom'
+import AuthProvider from './context/AuthContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <HashRouter>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </HashRouter>
   </React.StrictMode>
 )
